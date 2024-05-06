4a6ae24b0f01ecaa0062769951add6c88ce3a8bb
Eloi Gattet <eloi.gattet@mercurioimaging.com>
2024-05-06 14:06:40 +0200

    SD OK

--- CNRS_MAP_UUID_V1.ino
+++ CNRS_MAP_UUID_V1.ino
@@ -1,8 +1,14 @@
 #define PAPERDINK_DEVICE Paperdink_Classic  // Définir avant toute inclusion
 #include <Paperdink.h>
-#include <TimeLib.h>
 #include "UUID.h"
 #include "qrcode_gen.h"
+#include <SPI.h>
+#include <SD.h>
+#include "Button.h"
+#include "config.h"
+
+
+const char* version = "V1.1 - Test SD";
 
 
 UUID uuid;
@@ -10,13 +16,16 @@ QRCode qrcode;
 PAPERDINK_DEVICE Paperdink;
 
 GxEPD2_GFX& display = Paperdink.epd;
+int screenWidth = 400;  // La largeur de l'écran
+int charWidth = 6;      // Largeur estimée d'un caractère en pixels
 
-// Définition des pins des boutons
-#define BUTTON_1_PIN 14
 
 void setup() {
+  Serial.begin(115200);
 
+  //REGLER L'HEURE :
   pinMode(BUTTON_1_PIN, INPUT_PULLUP);  // Configure le bouton avec une résistance de rappel interne
+  pinMode(SD_EN, OUTPUT);
 
   // Initialisation de l'écran
   Paperdink.begin();
@@ -24,49 +33,76 @@ void setup() {
   Paperdink.epd.fillScreen(GxEPD_WHITE);
   Paperdink.epd.setTextColor(GxEPD_BLACK);
 
-  display.setCursor(50, display.height() / 2);
-  display.println("Pret a generer UUID");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Initialisation de la carte SD échouée!");
+    return;  // Arrêter ici si échec
+  }
+  Serial.println("Initialisation de la carte SD réussie.");
+
+
+  int textlenght = strlen(version) * charWidth;
+  int uuidTextX = (screenWidth - textlenght) / 2;
+  display.setCursor(uuidTextX, display.height() / 2);
+  display.println(version);
   Paperdink.epd.display();
 }
 
 
 void loop() {
   if (digitalRead(BUTTON_1_PIN) == LOW) {
+    Paperdink.epd.fillScreen(GxEPD_WHITE);
     generateAndDisplayUUID();
     delay(200);  // Anti-rebond
-    while(digitalRead(BUTTON_1_PIN) == LOW);  // Attendre que le bouton soit relâché
+    while (digitalRead(BUTTON_1_PIN) == LOW)
+      ;  // Attendre que le bouton soit relâché
   }
 }
 
 
 void generateAndDisplayUUID() {
-  uuid.generate();  // Générer un nouvel UUID
+  uuid.generate();                        // Générer un nouvel UUID
   char* uuidString = uuid.toCharArray();  // Obtenir la chaîne UUID
+  int uuidTextWidth = strlen("UUID:") * charWidth;
+  int uuidValueWidth = strlen(uuidString) * charWidth;
 
-  display.setCursor(0, 50);
-  display.println("UUID:");
-  display.println(uuidString);  // Affichez la chaîne correctement obtenue
-  Paperdink.epd.display();
-  delay(500);
-  generateQRCode(uuidString);  // Passez cette chaîne au générateur de QR Code
+  // Calculer la position x pour centrer le texte "UUID:"
+  int uuidTextX = (screenWidth - uuidTextWidth) / 2;
+  // Calculer la position x pour centrer la chaîne UUID
+  int uuidValueX = (screenWidth - uuidValueWidth) / 2;
+
+
+  // Positionner et afficher la valeur UUID
+  display.setCursor(uuidValueX, 20);
+  display.println(uuidString);
+  digitalWrite(SD_EN, LOW);
+
+  File file = SD.open("/historique.txt", FILE_APPEND);
+  if (file) {
+    file.println(uuidString);  // Écrire l'UUID suivi d'un saut de ligne
+    file.close();              // Fermer le fichier après écriture
+    Serial.print("UUID ajouté à 'historique.txt': ");
+    Serial.println(uuidString);
+  } else {
+    Serial.println("Erreur : Impossible d'ouvrir 'historique.txt' en mode ajout.");
+  }
+
+  // Générer et afficher le QR Code
+  generateQRCode(uuidString);
+  digitalWrite(SD_EN, HIGH);
 }
 
 
-void generateQRCode(const char *text) {
+void generateQRCode(const char* text) {
   uint8_t qrcodeData[qrcode_getBufferSize(3)];
   qrcode_initText(&qrcode, qrcodeData, 3, 0, text);
-
-  // Appeler la fonction d'affichage du QR Code
   displayQRCode(qrcode);
 }
 
-void displayQRCode(QRCode &qrcode) {
+void displayQRCode(QRCode& qrcode) {
   int size = qrcode.size;
-  int scale = 2;  // Ajuster la taille des modules pour un meilleur affichage
+  int scale = 8;  // Ajuster la taille des modules pour un meilleur affichage
   int top = (display.height() - size * scale) / 2;
   int left = (display.width() - size * scale) / 2;
-
-  display.fillScreen(GxEPD_WHITE);  // Effacer l'écran
   for (int y = 0; y < size; y++) {
     for (int x = 0; x < size; x++) {
       if (qrcode_getModule(&qrcode, x, y)) {
---
+++ config.h
@@ -0,0 +1,69 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ENABLE_GxEPD2_GFX 1
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+#define PI_CLASSIC (true)
+
+
+#define SSID     "Bytel-E-Etamine-78058-2.4GHz"     // your network SSID (name of wifi network)
+#define PASSWORD "rxc0jmwrpNs6Hpbe"     // your network password
+#define TIME_ZONE "+07:00"      // Your time zone. Daylight saving not handled yet.
+#define START_DAY_OFFSET 0
+
+// PIN ASSIGNMENT
+
+// I2C pins
+#define SDA 16
+#define SCL 17
+
+// SPI pins
+
+// SD card pins
+#define SS 21
+#define SD_CS SS
+#define SD_EN 5
+
+// E-paper pins
+#define EPD_CS 22
+#define EPD_DC 15
+#define EPD_BUSY 34
+#define EPD_EN 12
+#define EPD_RST 13
+
+// PCF8574 pins
+#define PCF_INT 35
+#define SD_CD P4 // input
+#define EXT_GPIO1 P5
+#define EXT_GPIO2 P6
+#define EXT_GPIO3 P7
+#define PCF_I2C_ADDR 0x38
+
+// LiPo
+#define CHARGING_PIN 36
+#define BATT_EN 25
+#define BATTERY_VOLTAGE 39
+
+// Buzzer
+#define BUZR 26
+
+// Buttons
+// Top to bottom
+#define BUTTON_1_PIN 14
+#define BUTTON_1_RTC_GPIO 16
+
+#define BUTTON_2_PIN 27
+#define BUTTON_2_RTC_GPIO 17
+
+#define BUTTON_3_PIN 4
+#define BUTTON_3_RTC_GPIO 10
+
+#define BUTTON_4_PIN 2
+#define BUTTON_4_RTC_GPIO 12
+
+#define DEBOUNCE_TIME 10
+
+#endif /* CONFIG_H */
