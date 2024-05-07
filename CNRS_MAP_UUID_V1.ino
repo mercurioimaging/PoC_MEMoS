0670056160277a5f2f9016831f320feff72d956c
Eloi Gattet <eloi.gattet@mercurioimaging.com>
2024-05-07 15:31:51 +0200

    V1.4 - Menu + detection SD en cours

--- CNRS_MAP_UUID_V1.ino
+++ CNRS_MAP_UUID_V1.ino
@@ -11,7 +11,7 @@
 
 
 
-const char* version = "V1.3 - HUD + DeepSleep";
+const char* version = "V1.4 - Gestion NO SD";
 
 
 UUID uuid;
@@ -75,8 +75,9 @@ void setup() {
   int uuidTextX = (screenWidth - textlenght) / 2;
   display.setCursor(uuidTextX, display.height() / 2);
   display.println(version);
+  HUD();
+  Menu();
   Paperdink.epd.display();
-
 
   lastInteractionTime = millis();
 }
@@ -104,155 +105,164 @@ void loop() {
 }
 
 
-  void generateAndDisplayUUID() {
-    uuid.generate();                        // Générer un nouvel UUID
-    char* uuidString = uuid.toCharArray();  // Obtenir la chaîne UUID
+void generateAndDisplayUUID() {
+  uuid.generate();                        // Générer un nouvel UUID
+  char* uuidString = uuid.toCharArray();  // Obtenir la chaîne UUID
 
-    digitalWrite(SD_EN, LOW);
-    if (mots.size() < 3) {
-      Serial.println("Pas assez de mots dans le fichier.");
-      return;
-    }
-
-    // Sélectionner aléatoirement trois mots
-    String selectedWords[3];
-    for (int i = 0; i < 3; i++) {
-      int index = random(mots.size());  // Choix aléatoire d'un index
-      selectedWords[i] = mots[index];
-    }
-
-    // Écrire l'UUID et les mots choisis dans "historique.txt"
-    File file = SD.open("/historique.txt", FILE_APPEND);
-    if (file) {
-      file.print(uuidString);
-      file.println("," + selectedWords[0] + "-" + selectedWords[1] + "-" + selectedWords[2]);
-      file.close();
-
-      Serial.print("UUID et mots enregistrés : ");
-      Serial.print(uuidString);
-      Serial.print(" ");
-      Serial.println(selectedWords[0] + "-" + selectedWords[1] + "-" + selectedWords[2]);
-    } else {
-      Serial.println("Erreur : Impossible d'ouvrir 'historique.txt' en mode ajout.");
-
-      //TODO : Afficher un warning dans le HUD
-    }
-
-    // Générer et afficher le QR Code
-    displayUUIDandQRCode(uuidString, selectedWords);
-    //Libérer la carte SD
-    digitalWrite(SD_EN, HIGH);
+  digitalWrite(SD_EN, LOW);
+  if (mots.size() < 3) {
+    Serial.println("Pas assez de mots dans le fichier.");
+    return;
   }
 
+  // Sélectionner aléatoirement trois mots
+  String selectedWords[3];
+  for (int i = 0; i < 3; i++) {
+    int index = random(mots.size());  // Choix aléatoire d'un index
+    selectedWords[i] = mots[index];
+  }
 
-  void displayUUIDandQRCode(const char* uuidString, const String words[]) {
-    // Génération du QR Code
-    QRCode qrcode;
-    uint8_t qrcodeData[qrcode_getBufferSize(3)];
-    qrcode_initText(&qrcode, qrcodeData, 3, 0, uuidString);
+  // Écrire l'UUID et les mots choisis dans "historique.txt"
+  File file = SD.open("/historique.txt", FILE_APPEND);
+  if (file) {
+    file.print(uuidString);
+    file.println("," + selectedWords[0] + "-" + selectedWords[1] + "-" + selectedWords[2]);
+    file.close();
+
+    Serial.print("UUID et mots enregistrés : ");
+    Serial.print(uuidString);
+    Serial.print(" ");
+    Serial.println(selectedWords[0] + "-" + selectedWords[1] + "-" + selectedWords[2]);
+  } else {
+    Serial.println("Erreur : Impossible d'ouvrir 'historique.txt' en mode ajout.");
+
+    //TODO : Afficher un warning dans le HUD
+  }
+
+  // Générer et afficher le QR Code
+  displayUUIDandQRCode(uuidString, selectedWords);
+  //Libérer la carte SD
+  digitalWrite(SD_EN, HIGH);
+}
 
 
-    /// AFFICHER LE TEXTE DE L'UUID
-    /*############################################################*/
-    int uuidValueWidth = strlen(uuidString) * charWidth;
-    int uuidValueX = (screenWidth - uuidValueWidth) / 2;
-    // Positionner et afficher la valeur UUID
-    display.setCursor(uuidValueX, 20);
-    display.println(uuidString);
+void displayUUIDandQRCode(const char* uuidString, const String words[]) {
+  // Génération du QR Code
+  QRCode qrcode;
+  uint8_t qrcodeData[qrcode_getBufferSize(3)];
+  qrcode_initText(&qrcode, qrcodeData, 3, 0, uuidString);
 
 
-    /// AFFICHER LE QR CODE
-    /*############################################################*/
-    int size = qrcode.size;
-    int scale = 8;  // Ajuster la taille des modules pour un meilleur affichage
-    int top = (display.height() - size * scale) / 2;
-    int left = (display.width() - size * scale) / 2;
-    for (int y = 0; y < size; y++) {
-      for (int x = 0; x < size; x++) {
-        if (qrcode_getModule(&qrcode, x, y)) {
-          display.fillRect(left + x * scale, top + y * scale, scale, scale, GxEPD_BLACK);
-        }
+  /// AFFICHER LE TEXTE DE L'UUID
+  /*############################################################*/
+  int uuidValueWidth = strlen(uuidString) * charWidth;
+  int uuidValueX = (screenWidth - uuidValueWidth) / 2;
+  // Positionner et afficher la valeur UUID
+  display.setCursor(uuidValueX, 20);
+  display.println(uuidString);
+
+
+  /// AFFICHER LE QR CODE
+  /*############################################################*/
+  int size = qrcode.size;
+  int scale = 8;  // Ajuster la taille des modules pour un meilleur affichage
+  int top = (display.height() - size * scale) / 2;
+  int left = (display.width() - size * scale) / 2;
+  for (int y = 0; y < size; y++) {
+    for (int x = 0; x < size; x++) {
+      if (qrcode_getModule(&qrcode, x, y)) {
+        display.fillRect(left + x * scale, top + y * scale, scale, scale, GxEPD_BLACK);
       }
     }
+  }
 
-    /// AFFICHER LE TRIPTYQUE
-    /*############################################################*/
-    // Préparation à l'affichage des mots
-    String combinedWords = words[0] + " - " + words[1] + " - " + words[2];
-    int textWidth = combinedWords.length() * charWidth;
-    int textX = (screenWidth - textWidth) / 2;
-    int textY = top + size * scale + 20;  // 20 pixels sous le QR code
-    // Mise en place du curseur
-    display.setCursor(textX, textY);
-    display.println(combinedWords);
+  /// AFFICHER LE TRIPTYQUE
+  /*############################################################*/
+  // Préparation à l'affichage des mots
+  String combinedWords = words[0] + " - " + words[1] + " - " + words[2];
+  int textWidth = combinedWords.length() * charWidth;
+  int textX = (screenWidth - textWidth) / 2;
+  int textY = top + size * scale + 20;  // 20 pixels sous le QR code
+  // Mise en place du curseur
+  display.setCursor(textX, textY);
+  display.println(combinedWords);
 
-    //On appelle le HUD
-    HUD();
-    Menu();
-    display.display();  // Mettre à jour l'affichage
+  //On appelle le HUD
+  HUD();
+  Menu();
+  display.display();  // Mettre à jour l'affichage
+}
+
+
+void HUD() {
+  int x = 3;
+  int y = 3;
+
+  // Déterminer le niveau de la batterie et si elle est en charge
+  uint8_t charging = digitalRead(CHARGING_PIN) ^ 1;
+  digitalWrite(BATT_EN, LOW);
+  delay(10);
+  analogReadResolution(12);
+  int adc = analogReadMilliVolts(BATTERY_VOLTAGE);
+  digitalWrite(BATT_EN, HIGH);
+  double vbat = (double(adc) * 1.29375) / 1000;  // Calcul du voltage de la batterie
+
+  // Choix de l'icône de la batterie à afficher
+  const uint8_t* batt_icon;
+  int batt_width, batt_height;
+  if (vbat < 3.5) {
+    batt_icon = Batt_Low_bits;
+    batt_width = Batt_Low_width;
+    batt_height = Batt_Low_height;
+  } else if (vbat < 3.7) {
+    batt_icon = Batt_33_bits;
+    batt_width = Batt_33_width;
+    batt_height = Batt_33_height;
+  } else if (vbat < 3.9) {
+    batt_icon = Batt_66_bits;
+    batt_width = Batt_66_width;
+    batt_height = Batt_66_height;
+  } else {
+    batt_icon = Batt_100_bits;
+    batt_width = Batt_100_width;
+    batt_height = Batt_100_height;
+  }
+  // Affichage de l'icône de la batterie
+  display.drawXBitmap(x, y, (const uint8_t*)batt_icon, batt_width, batt_height, GxEPD_BLACK);
+
+  x += batt_width + 3;  // Décaler x pour placer l'icône de charge à droite de la batterie
+
+  // Si la batterie est en charge, afficher l'icône de charge à droite de l'icône du niveau de batterie
+  if (charging) {
+    display.drawXBitmap(x, y, (const uint8_t*)charge_bits, charge_width, charge_height, GxEPD_BLACK);
   }
 
 
-  void HUD() {
-    int x = 3;
-    int y = 3;
+  // Calcul pour la position de l'icône de la carte SD (juste en dessous de l'icône de la batterie)
+  y += batt_height + 5;  // Ajouter une marge entre les icônes
 
-    // Déterminer le niveau de la batterie et si elle est en charge
-    uint8_t charging = digitalRead(CHARGING_PIN) ^ 1;
-    digitalWrite(BATT_EN, LOW);
-    delay(10);
-    analogReadResolution(12);
-    int adc = analogReadMilliVolts(BATTERY_VOLTAGE);
-    digitalWrite(BATT_EN, HIGH);
-    double vbat = (double(adc) * 1.29375) / 1000;  // Calcul du voltage de la batterie
-
-    // Choix de l'icône de la batterie à afficher
-    const uint8_t* batt_icon;
-    int batt_width, batt_height;
-    if (charging) {
-      batt_icon = Batt_charge_bits;
-      batt_width = Batt_charge_width;
-      batt_height = Batt_charge_height;
-    } else if (vbat < 3.5) {
-      batt_icon = Batt_Low_bits;
-      batt_width = Batt_Low_width;
-      batt_height = Batt_Low_height;
-    } else if (vbat < 3.7) {
-      batt_icon = Batt_33_bits;
-      batt_width = Batt_33_width;
-      batt_height = Batt_33_height;
-    } else if (vbat < 3.9) {
-      batt_icon = Batt_66_bits;
-      batt_width = Batt_66_width;
-      batt_height = Batt_66_height;
-    } else {
-      batt_icon = Batt_100_bits;
-      batt_width = Batt_100_width;
-      batt_height = Batt_100_height;
-    }
-
-    // Affichage de l'icône de la batterie
-    display.drawXBitmap(x, y, (const uint8_t*)batt_icon, batt_width, batt_height, GxEPD_BLACK);
-
-    // Calcul pour la position de l'icône de la carte SD (juste en dessous de l'icône de la batterie)
-    y += batt_height + 5;  // Ajouter une marge entre les icônes
-
-    // Affichage de l'icône de la carte SD
-    if (sdPresent) {
-      display.drawXBitmap(x, y, (const uint8_t*)sd_20px_bits, sd_20px_width, sd_20px_height, GxEPD_BLACK);
-    } else {
-      display.drawXBitmap(x, y, (const uint8_t*)nosd_20px_bits, nosd_20px_width, nosd_20px_height, GxEPD_BLACK);
-    }
-
-    y += sd_20px_height + 5;
-
-    // Affichage de l'icône de veille si l'appareil est en mode veille
-    if (sleepStatus) {
-      display.drawXBitmap(x, y, (const uint8_t*)sleep_bits, sleep_width, sleep_height, GxEPD_BLACK);
-    }
+  x = 3;
+  // Affichage de l'icône de la carte SD
+  if (sdPresent) {
+    display.drawXBitmap(x, y, (const uint8_t*)sd_20px_bits, sd_20px_width, sd_20px_height, GxEPD_BLACK);
+  } else {
+    display.drawXBitmap(x, y, (const uint8_t*)nosd_20px_bits, nosd_20px_width, nosd_20px_height, GxEPD_BLACK);
   }
 
-  void Menu() {
-    // Affichage de l'icône de "power" à la position spécifiée
-    display.drawXBitmap(359, 252, (const uint8_t*)power_bits, power_width, power_height, GxEPD_BLACK);
+  y += sd_20px_height + 5;
+  x = 3;
+  // Affichage de l'icône de veille si l'appareil est en mode veille
+  if (sleepStatus) {
+    display.drawXBitmap(x, y, (const uint8_t*)sleep_bits, sleep_width, sleep_height, GxEPD_BLACK);
   }
+}
+
+void Menu() {
+  //Affichage de l'icone "new"
+ display.drawXBitmap(380, 280, (const uint8_t*)new_bits, new_width, new_height, GxEPD_BLACK);
+  //Affichage de l'icone "historique"
+  display.drawXBitmap(380, 100, (const uint8_t*)history_bits, history_width, history_height, GxEPD_BLACK);
+  // Affichage de l'icône de "power" à la position spécifiée
+  display.drawXBitmap(380, 280, (const uint8_t*)power_bits, power_width, power_height, GxEPD_BLACK);
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# MEMoS_POC
-e-ink (paperd.ink) based UUID Qr-Code Generator
+#MEMoS - Proof of Concept
+
+## Description
+
+Ce projet implémente un générateur d'identifiants uniques universels (UUID) utilisant un ESP32 et un écran e-ink. Le dispositif permet de générer des UUID aléatoires qui sont ensuite affichés sous forme de QRcode sur l'écran. 
+L'état de la batterie, la présence de la carte SD et d'autres informations systèmes sont également affichés à l'aide d'icônes sur l'écran.
+
+## Fonctionnalités
+
+- **Génération d'UUID** : Génère des UUID conformes à la norme RFC 4122.
+- **Affichage de QR Code** : Affiche l'UUID généré sous forme de QR Code pour une utilisation facile.
+- **Surveillance de la batterie** : Affiche le niveau actuel de la batterie et son état de charge.
+- **Détection de carte SD** : Indique si une carte SD est présente et active.
+- **Mode veille** : Bascule en mode veille après une minute d'inactivité ou par pression d'un bouton pour économiser de l'énergie.
+- **Réveil par bouton** : Permet le réveil de l'appareil par pression sur un bouton dédié.
+
+## Matériel Requis
+
+- Écran e-ink PAPERD.ink Classic
+- Carte SD (optionnelle pour étendre la mémoire)
+- Batterie avec gestion de charge
+- Boutons de navigation
+
+## Configuration et Déploiement
+
+### Installation Logicielle
+
+1. Installez l'IDE Arduino et configurez-le pour le développement avec ESP32. (Carte : ESP32 Dev Module). 
+2. Téléchargez les bibliothèques nécessaires (QRCode, GxEPD, etc.) via le gestionnaire de bibliothèques de l'IDE Arduino. Suivre le tutoriel https://docs.paperd.ink/docs/software/getting-started/
+3. Clonez le dépôt du projet ou téléchargez les fichiers sources.
+4. Ouvrez le fichier principal du projet dans l'IDE Arduino et téléversez-le sur votre ESP32.
+
+## Utilisation
+
+- Allumez l'appareil en appuyant sur le bouton de mise en marche (en bas à droite)
+- Utilisez les boutons pour naviguer dans les menus et générer des UUID.
+- Surveillez l'état de la batterie et la présence de la carte SD via les icônes affichées.
